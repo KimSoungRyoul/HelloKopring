5b2663bfdb5fde6935e8fe586dbdbc5bc808a1cf
soungryoulkim <kimsoungryoul@gmail.com>
2022-04-20 16:54:37 +0900

    FF-138 gradle 라이브러리 버전 최신화

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.6.6"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.10"
-	kotlin("plugin.spring") version "1.6.10"
-	kotlin("plugin.jpa") version "1.6.10"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+
+	// Gradle Use Latest Versions Plugin
+	id("se.patrikerdes.use-latest-versions") version "0.2.18"
+	id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 group = "com.example"
@@ -36,8 +40,12 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-	implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:3.1.1")
-	implementation("org.springframework.cloud:spring-cloud-starter-aws-secrets-manager-config:2.2.6.RELEASE")
+	// SpringCloud AWS
+	implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE") //
+	//implementation("org.springframework.cloud:spring-cloud-starter-aws-secrets-manager-config") // :2.2.6.RELEASE
+	implementation("io.awspring.cloud:spring-cloud-aws-secrets-manager-config:2.4.1")
+	implementation("cloud.localstack:localstack-utils:0.2.20")
+	implementation("com.amazonaws:aws-java-sdk:1.12.202")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
@@ -46,11 +54,19 @@ dependencies {
 	testImplementation("org.springframework.security:spring-security-test")
 }
 
+
 dependencyManagement {
 	imports {
 		mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
 	}
 }
+
+allOpen { // 추가적으로 열어줄 allOpen
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
 
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
