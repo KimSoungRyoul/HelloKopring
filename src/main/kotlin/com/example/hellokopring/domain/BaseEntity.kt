f9b721c2d3caa8b7a6cf802ec5288ee5f490b908
soungryoulkim <kimsoungryoul@gmail.com>
2022-04-26 09:51:09 +0900

    FF-137 BaseEntity.kt abstract 추가

--- src/main/kotlin/com/example/hellokopring/domain/BaseEntity.kt
+++ src/main/kotlin/com/example/hellokopring/domain/BaseEntity.kt
@@ -4,19 +4,23 @@ import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import javax.persistence.Column
-import javax.persistence.EntityListeners
-import javax.persistence.MappedSuperclass
+import javax.persistence.*
 
 @MappedSuperclass
 @EntityListeners(value = [AuditingEntityListener::class])
-class BaseEntity {
+abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
 
     @CreatedDate
-    @Column(name = "created_datetime", insertable = false, updatable = false)
-    lateinit var createdDateTime: LocalDateTime
+    @Column(name = "created_datetime", nullable = false, updatable = false)
+    var createdDateTime: LocalDateTime = LocalDateTime.now()
+        protected set
 
     @LastModifiedDate
-    @Column(name = "updated_datetime", insertable = false, updatable = false)
-    lateinit var updatedDateTime: LocalDateTime
+    @Column(name = "updated_datetime", nullable = false)
+    var updatedDateTime: LocalDateTime = LocalDateTime.now()
+        protected set
+
 }
